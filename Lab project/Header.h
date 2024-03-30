fc68b1adc771a79461452572e17bb8fcc6df8a93
Artem Karp <162900678+CuteDreamer@users.noreply.github.com>
2024-03-30 14:13:23 +0200

    Add files via upload

---
+++ Lab project/Header.h
@@ -0,0 +1,9 @@
+
+
+using namespace std;
+
+/// задаём соответствия нажатиям клавиш
+enum direction { DOWN = 80, UP = 72, LEFT = 75, RIGHT = 77 };
+
+/// задаём соответствия цветам
+enum color { green = 33, white = 39, black = 32 };
